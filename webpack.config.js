4661332ae431560647e35125fbb373c9da439ccf
kimmingyo8 <kimmingyo8@naver.com>
2023-11-26 21:31:22 +0900

    tailwind init

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	// 로더 등록
+	module: {
+		rules: [
+			{
+				test: /\.css?$/,
+				use: ['style-loader', 'css-loader', 'postcss-loader'],
+				exclude: ['/node_modules/'],
+			},
+		],
+	},
+};
