0cf2ac465e087c3e4123c379ede542fa789f024d
kimmingyo8 <kimmingyo8@naver.com>
2023-11-27 01:09:43 +0900

    설문지 생성 페이지(1) 및 input 컴포넌트 UI

---
+++ src/components/common/Input.tsx
@@ -0,0 +1,23 @@
+import { InputHTMLAttributes } from 'react';
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+	name: string;
+	placeholder?: string;
+	className?: string;
+}
+
+const Input = ({ name, placeholder, className, ...props }: InputProps) => {
+	return (
+		<label className="w-full">
+			<input
+				type="text"
+				name={name}
+				placeholder={placeholder}
+				className={`w-full text-gray-900 transition duration-300 border-b focus:border-lime-600 focus:outline-none ${className}`}
+				{...props}
+			/>
+		</label>
+	);
+};
+
+export default Input;
--- src/pages/form/new.tsx
+++ src/pages/form/new.tsx
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { ChangeEvent, useState } from 'react';
+import Input from '../../components/common/Input';
 
 const NewFormPage = () => {
-	return <div>NewFormPage</div>;
+	const [title, setTitle] = useState('');
+	const [desc, setDesc] = useState('');
+
+	const onInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+		const { name, value } = e.target;
+	};
+
+	return (
+		<form>
+			<Input
+				name="form-title"
+				placeholder="설문지 제목"
+				value={title}
+				className="p-3 text-4xl font-semibold"
+				onChange={onInputChange}
+			/>
+			<Input
+				name="form-desc"
+				placeholder="설문지 설명"
+				value={desc}
+				className="p-3 mt-1 text-sm text-gray-600 "
+				onChange={onInputChange}
+			/>
+		</form>
+	);
 };
 
 export default NewFormPage;
